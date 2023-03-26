198f77b8fe77ef9118eb1d9f600af3a4f0cf84eb
itizawa <itizawa.pen@gmail.com>
2023-03-26 19:53:15 +0900

    ♻️

--- packages/front/src/hooks/Page/usePages/usePages.ts
+++ packages/front/src/hooks/Page/usePages/usePages.ts
@@ -1,12 +1,12 @@
 import { Page } from '@wiscro/common';
-import useSWR from 'swr';
+import useSWR, { SWRResponse } from 'swr';
 import { restClient } from '../../../libs/restClient';
 
 /**
  * ページ一覧を取得するSWR
- * @returns {Page[]} pages
+ * @returns {SWRResponse<Page[]>} pages
  */
-export const usePages = () => {
+export const usePages = (): SWRResponse<Page[]> => {
   return useSWR(`/api/pages`, (endpoint: string) =>
     restClient.apiGet<{ pages: Page[] }>(endpoint).then((result) => {
       return result.data.pages.map((page) => new Page(page));
