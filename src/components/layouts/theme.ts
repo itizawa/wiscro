a52a12373279d2e2f8e3039a9434791c70b9bf61
itizawa <itizawa.pen@gmail.com>
2023-09-18 20:20:55 +0900

    DefaultLayoutを使用

---
+++ src/components/layouts/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#90ADC6',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+  },
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,37 +7,23 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
-import Link from 'next/link';
-import { URLS } from '~/constants/urls';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '~/components/layouts/theme';
+import { DefaultLayout } from '~/components/layouts/DefaultLayout';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function MyApp({ Component, pageProps }: any) {
   return (
     // TODO: display toaster
     <SWRConfig value={{ onError: () => void 0 }}>
-      <Head>
-        <title>Wiscro</title>
-      </Head>
-      <Box sx={{ minHeight: '100vh' }}>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <Link className="navbar-brand text-decoration-none text-black" href={URLS.TOP}>
-                Wiscro
-              </Link>
-              <Link href={URLS.QUESTION_NEW}>
-                <Button type="button" className="btn btn-md btn-primary text-white">
-                  作成
-                </Button>
-              </Link>
-            </Toolbar>
-          </AppBar>
-        </Box>
-        <div className="container">
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Wiscro</title>
+        </Head>
+        <DefaultLayout>
           <Component {...pageProps} />
-        </div>
-      </Box>
+        </DefaultLayout>
+      </ThemeProvider>
     </SWRConfig>
   );
 }
