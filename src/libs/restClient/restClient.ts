7595d8be1148efd733020be094c59aed395148cd
itizawa <itizawa.pen@gmail.com>
2023-09-23 20:37:02 +0900

    RestClient の改善

--- src/libs/restClient/restClient.ts
+++ src/libs/restClient/restClient.ts
@@ -1,24 +1,69 @@
+import urlJoin from 'url-join';
+
 class RestClient {
   static baseHeaders = {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
     'Access-Control-Allow-Credentials': true,
   };
+  async handler<T>(
+    path: string,
+    method: 'GET' | 'POST' | 'PATCH' | 'DELETE',
+    options?: {
+      body?: object;
+      queryParams?: object;
+      fileDownload?: {
+        isFileDownload: boolean;
+        fileName?: string;
+      };
+    },
+  ): Promise<T> {
+    const url = urlJoin(process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8080/', path);
 
-  async apiGet(url: string, init?: RequestInit | undefined): Promise<Response> {
-    return await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}${url}`, { ...RestClient.baseHeaders, ...init });
+    const headers: HeadersInit = {
+      'Content-Type': 'application/json',
+    };
+
+    const init: RequestInit = {
+      method,
+      headers,
+    };
+
+    if (options?.body) {
+      // NOTE: DATE型の値をJSONに変換すると文字列になってしまうため、JSON.stringifyではなくsuperjson.stringifyを使っている
+      init.body = options?.body ? JSON.stringify(options.body) : undefined;
+    }
+
+    const response = await fetch(url, init).catch((error) => {
+      console.error(error);
+      throw new Error(error);
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    }
+
+    console.error('response.ok:', response.ok);
+    console.error('esponse.status:', response.status);
+    console.error('esponse.statusText:', response.statusText);
+    throw new Error(response.statusText);
   }
 
-  async apiPost(url: string, body?: BodyInit | null): Promise<Response> {
-    return await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}${url}`, { ...RestClient.baseHeaders, method: 'POST', body });
+  async apiGet<T>(url: string, query?: object): Promise<T> {
+    return await this.handler(url, 'GET', query);
   }
 
-  async apiPatch(url: string, body?: BodyInit | null): Promise<Response> {
-    return await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}${url}`, { ...RestClient.baseHeaders, method: 'PATCH', body });
+  async apiPost<T>(url: string, body?: object): Promise<T> {
+    return await this.handler(url, 'POST', { body });
   }
 
-  async apiDelete(url: string, body?: BodyInit | null): Promise<Response> {
-    return await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}${url}`, { ...RestClient.baseHeaders, method: 'DELETE', body });
+  async apiPatch<T>(url: string, body?: object): Promise<T> {
+    return await this.handler(url, 'PATCH', { body });
+  }
+
+  async apiDelete<T>(url: string, body?: object): Promise<T> {
+    return await this.handler(url, 'DELETE', { body });
   }
 }
 
