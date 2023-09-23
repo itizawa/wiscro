ecd4de0d7ccc46ded0fa3d7c59ff9728eaecad85
itizawa <itizawa.pen@gmail.com>
2023-09-23 16:15:33 +0900

    質問詳細ページを実装した

---
+++ src/app/questions/[id]/page.tsx
@@ -0,0 +1,11 @@
+export async function generateStaticParams() {
+  // TODO: 新着の質問は予め静的サイト生成しておく
+  return [];
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/questions/${params.id}`, { cache: 'force-cache' });
+  const data = await res.json();
+
+  return <div className="flex justify-center pt-[24px]">ここに詳細 {JSON.stringify(data)}</div>;
+}
