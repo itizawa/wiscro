46819b4a65b80651964c6acf1d2e3cf220276df8
itizawa <itizawa.pen@gmail.com>
2023-09-23 19:34:02 +0900

    style の修正

--- src/components/domains/Question/QuestionCard/QuestionCard.tsx
+++ src/components/domains/Question/QuestionCard/QuestionCard.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from 'react';
 import { Card } from '@nextui-org/react';
+import { format } from 'date-fns';
 import { Question } from '~/domains/Question';
 
 type Props = {
@@ -10,8 +11,9 @@ type Props = {
 
 export const QuestionCard: FC<Props> = ({ question }) => {
   return (
-    <Card className="w-[100%] max-w-[500px] p-[40px] flex flex-col gap-[24px]" shadow="sm">
-      <h4>{question.title}</h4>
+    <Card className="w-[100%] max-w-[500px] p-[40px] flex flex-col" shadow="sm">
+      <h4 className="font-bold text-lg mb-[8px]">{question.title}</h4>
+      <p className="text-slate-600 text-sm mb-[16px]">投稿日：{format(new Date(question.createdAt), 'yyyy/MM/dd HH:mm')}</p>
       <p>{question.description}</p>
     </Card>
   );
