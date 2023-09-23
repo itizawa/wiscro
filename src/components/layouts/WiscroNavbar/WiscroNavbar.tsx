b45dc17d674168e599065b71ae3e577f3baf5d05
itizawa <itizawa.pen@gmail.com>
2023-09-23 22:40:34 +0900

    ローディングを表示した

--- src/components/layouts/WiscroNavbar/WiscroNavbar.tsx
+++ src/components/layouts/WiscroNavbar/WiscroNavbar.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import React from 'react';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Spinner, Avatar } from '@nextui-org/react';
 import { URLS } from '~/constants/urls';
+import { useCurrentUser } from '~/hooks/user/useCurrentUser';
 
 export const WiscroNavbar = () => {
+  const { data: currentUser, isLoading } = useCurrentUser();
+
   return (
     <Navbar shouldHideOnScroll isBordered isBlurred={false}>
       <NavbarBrand>
@@ -13,14 +16,19 @@ export const WiscroNavbar = () => {
         </Link>
       </NavbarBrand>
       <NavbarContent justify="end">
-        <NavbarItem>
-          <Button as={Link} color="primary" href={URLS.QUESTION_NEW} variant="flat">
-            作成
-          </Button>
-          <Button as={Link} color="primary" href={URLS.LOGIN_TO_BACKEND} variant="flat">
-            ログイン
-          </Button>
-        </NavbarItem>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <NavbarItem>
+            {currentUser ? (
+              <Avatar src={currentUser.profileUrl} isBordered />
+            ) : (
+              <Button as={Link} color="primary" href={URLS.LOGIN_TO_BACKEND} variant="flat">
+                ログイン
+              </Button>
+            )}
+          </NavbarItem>
+        )}
       </NavbarContent>
     </Navbar>
   );
