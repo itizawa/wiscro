28f34710b7451871f677972c13b94d6cc5cf71b2
itizawa <itizawa.pen@gmail.com>
2023-09-24 22:48:22 +0900

    回答を促す画面を実装した

--- src/components/domains/Question/QuestionCard/QuestionCard.tsx
+++ src/components/domains/Question/QuestionCard/QuestionCard.tsx
@@ -1,20 +1,35 @@
 'use client';
 
 import { FC } from 'react';
-import { Card } from '@nextui-org/react';
+import { Avatar, AvatarIcon, Button, Card } from '@nextui-org/react';
 import { format } from 'date-fns';
 import { Question } from '~/domains/Question';
+import { useCurrentUser } from '~/hooks/user/useCurrentUser';
 
 type Props = {
   question: Question;
 };
 
 export const QuestionCard: FC<Props> = ({ question }) => {
+  const { data: currentUser } = useCurrentUser();
+
   return (
     <Card className="w-[100%] max-w-[500px] p-[40px] flex flex-col" shadow="sm">
       <h4 className="font-bold text-lg mb-[8px]">{question.title}</h4>
       <p className="text-slate-600 text-sm mb-[16px]">投稿日：{format(new Date(question.createdAt), 'yyyy/MM/dd HH:mm')}</p>
       <p>{question.description}</p>
+      <div className="flex flex-col justify-center items-center mt-[24px] pt-[40px] border-t-2">
+        <Avatar icon={<AvatarIcon />} src={currentUser?.profileUrl} isBordered />
+        <p className="mt-[16px] text-center">
+          {currentUser?.username || 'ゲストユーザー'}さん
+          <br />
+          この質問に回答してみませんか?
+        </p>
+        <p className="mt-[4px] text-sm text-slate-400">URLを貼り付けるだけで回答ができます</p>
+        <Button className="mt-[16px]" color="primary">
+          回答する
+        </Button>
+      </div>
     </Card>
   );
 };
