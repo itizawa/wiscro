68c456e6a330dcd6435ce6c8819b51928dd4db92
itizawa <itizawa.pen@gmail.com>
2023-09-28 00:55:14 +0900

    type の修正

--- src/domains/Answer/Answer.ts
+++ src/domains/Answer/Answer.ts
@@ -3,6 +3,12 @@ export interface Answer {
   url: string;
   createdUserId: string;
   questionId: string;
+  title: string;
+  description: string;
+  favicon?: string;
+  image?: string;
+  body?: string;
+  siteName?: string;
   createdAt: string;
   updatedAt: string;
 }
