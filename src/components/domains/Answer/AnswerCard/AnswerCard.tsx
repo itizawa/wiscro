167e1cc7f6dbcdb67d4df988f123da2ac293ff52
itizawa <itizawa.pen@gmail.com>
2023-09-29 18:26:36 +0900

    add asset

--- src/components/domains/Answer/AnswerCard/AnswerCard.tsx
+++ src/components/domains/Answer/AnswerCard/AnswerCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Image } from '@nextui-org/react';
 import { FC } from 'react';
+import { IMAGE } from '~/constants/images';
 import { Answer } from '~/domains/Answer';
 
 type Props = {
@@ -7,10 +8,10 @@ type Props = {
 };
 export const AnswerCard: FC<Props> = ({ answer }) => {
   return (
-    <Card>
+    <Card className="p-0">
       <CardBody>
         <a href={answer.url} target="blank" rel="noopener noreferrer">
-          <Image src={answer.image || ''} />
+          <Image width="100%" height="auto" src={answer.image || IMAGE.NO_IMAGE} fallbackSrc={IMAGE.NO_IMAGE} />
         </a>
       </CardBody>
     </Card>
---
+++ src/constants/images.ts
@@ -0,0 +1,3 @@
+export const IMAGE = {
+  NO_IMAGE: '/images/no-image.png',
+};
