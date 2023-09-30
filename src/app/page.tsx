f39e0eef827ec836134cd6ed48980fa87eab3b48
itizawa <itizawa.pen@gmail.com>
2023-09-30 13:01:24 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import { TopQuestionCard } from './_components/TopQuestionCard';
 import { Question } from '~/domains/Question';
 import { restClient } from '~/libs/restClient';
 
+export async function generateStaticParams() {
+  // TODO: 新着の質問は予め静的サイト生成しておく
+  return [];
+}
 export default async function Page() {
-  const data = await restClient.apiGet<{ questions: Question[] }>('/api/questions', { next: { revalidate: 60 } });
-  const questions = data?.questions ?? [];
+  const { questions } = await restClient.apiGet<{ questions: Question[] }>('/api/questions', { next: { revalidate: 60 } });
 
   return (
     <div className="flex justify-center items-center flex-col gap-[24px] pt-[24px] pb-[80px] px-3">
