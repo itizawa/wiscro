fbf6471a57951f948d8fcd6f68af11a0df846b72
itizawa <itizawa.pen@gmail.com>
2023-09-30 12:45:01 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import { Question } from '~/domains/Question';
 import { restClient } from '~/libs/restClient';
 
 export default async function Page() {
-  const { questions = [] } = await restClient.apiGet<{ questions: Question[] }>('/api/questions', { cache: 'no-cache' });
+  const data = await restClient.apiGet<{ questions: Question[] }>('/api/questions', { next: { revalidate: 60 } });
+  const questions = data?.questions ?? [];
+
   return (
-    <div className="flex justify-center items-center flex-col gap-[24px] pt-[24px] px-3">
+    <div className="flex justify-center items-center flex-col gap-[24px] pt-[24px] pb-[80px] px-3">
       {questions.map((question) => (
         <TopQuestionCard key={question._id} question={question} />
       ))}
