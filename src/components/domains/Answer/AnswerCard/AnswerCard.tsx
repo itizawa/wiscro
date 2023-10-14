20a772e0c547414d0cb54a3f0933d37dbf7910fa
itizawa <itizawa.pen@gmail.com>
2023-10-14 22:46:04 +0900

    サマリーを表示する

--- src/components/domains/Answer/AnswerCard/AnswerCard.tsx
+++ src/components/domains/Answer/AnswerCard/AnswerCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, CardHeader, Image } from '@nextui-org/react';
 import { format } from 'date-fns';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import styles from './AnswerCard.module.css';
 import { IMAGE } from '~/constants/images';
 import { Answer } from '~/domains/Answer';
@@ -9,6 +9,8 @@ type Props = {
   answer: Answer;
 };
 export const AnswerCard: FC<Props> = ({ answer }) => {
+  const [isShowMore, setIsShowMore] = useState(false);
+
   return (
     <Card className="p-0">
       <CardHeader className="p-0">
@@ -25,7 +27,11 @@ export const AnswerCard: FC<Props> = ({ answer }) => {
       </CardHeader>
       <CardBody>
         <p className="font-bold mb-[4px]">{answer.title}</p>
-        <p className="text-slate-600 text-sm">回答日：{format(new Date(answer.createdAt), 'yyyy/MM/dd HH:mm')}</p>
+        <p className="text-slate-600 text-sm mb-[8px]">回答日：{format(new Date(answer.createdAt), 'yyyy/MM/dd HH:mm')}</p>
+        <p className={`mb-[4px] ${isShowMore ? '' : 'line-clamp-3'}`}>{answer.body}</p>
+        <p className="cursor-pointer text-sky-400" onClick={() => setIsShowMore((prev) => !prev)}>
+          {isShowMore ? '折りたたむ' : 'もっと見る'}
+        </p>
       </CardBody>
     </Card>
   );
