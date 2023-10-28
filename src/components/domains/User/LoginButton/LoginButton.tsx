4cc898d5b5b8cf9b0ccee5a78a38dae2206654fb
itizawa <itizawa.pen@gmail.com>
2023-10-28 22:30:38 +0900

    wip

--- src/components/domains/User/LoginButton/LoginButton.tsx
+++ src/components/domains/User/LoginButton/LoginButton.tsx
@@ -3,14 +3,17 @@
 import { Button } from '@nextui-org/button';
 import { FC, useEffect } from 'react';
 import { useDisclosure } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 import { LoginModal } from '../LoginModal';
 import { apiGetForC } from '~/app/restClient/restClientForC';
 
 export const LoginButton: FC = () => {
+  const router = useRouter();
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   useEffect(() => {
     apiGetForC('/api/me');
+    router.refresh();
   }, []);
 
   return (
