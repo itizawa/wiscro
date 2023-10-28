15028e51cc0bd893df6a49316e356fe6c4f52f45
itizawa <itizawa.pen@gmail.com>
2023-10-28 21:46:04 +0900

    delete

--- src/components/domains/User/PersonalDropdown/PersonalDropdown.tsx
+++ src/components/domains/User/PersonalDropdown/PersonalDropdown.tsx
@@ -1,20 +1,13 @@
 'use client';
 
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, useDisclosure } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
 import { EditNoteModal } from '../../Note/EditNoteModal';
 import { Icon } from '~/components/uiParts/icons';
 import { User } from '~/domains/User';
-import { apiGet } from '~/app/restClient';
 
 export const PersonalDropdown: FC<{ currentUser: User }> = ({ currentUser }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const router = useRouter();
-  useEffect(() => {
-    apiGet('/api/me');
-    router.refresh();
-  }, [router]);
 
   return (
     <>
