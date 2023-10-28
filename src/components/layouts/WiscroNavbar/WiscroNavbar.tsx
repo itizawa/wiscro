5849974f4ba67b201e651229b01fee9942ef0290
itizawa <itizawa.pen@gmail.com>
2023-10-28 19:46:48 +0900

    サーバーコンポーネント化

--- src/components/layouts/WiscroNavbar/WiscroNavbar.tsx
+++ src/components/layouts/WiscroNavbar/WiscroNavbar.tsx
@@ -1,7 +1,6 @@
-'use client';
-
 import React, { FC } from 'react';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from '@nextui-org/react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/navbar';
+import { Link } from '@nextui-org/link';
 import { LoginButton } from '~/components/domains/User/LoginButton';
 import { User } from '~/domains/User';
 import { PersonalDropdown } from '~/components/domains/User/PersonalDropdown';
