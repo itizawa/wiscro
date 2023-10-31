2cb282ae8bd87b9db36abdfd7cf8880a919b7321
itizawa <itizawa.pen@gmail.com>
2023-11-01 00:44:34 +0900

    Revert "Revert "wip""
    This reverts commit 770a0a0b8034766cdd3097b2a6c39f4ee871f858.

--- src/app/restClient/restClient.ts
+++ src/app/restClient/restClient.ts
@@ -1,20 +1,11 @@
-'use server';
-
 import urlJoin from 'url-join';
-import { cookies } from 'next/headers';
-export const handler = async <T>(path: string, method: 'GET' | 'POST' | 'PATCH' | 'DELETE', options?: RequestInit): Promise<T> => {
-  const cookieStore = cookies();
-  const cookie = cookieStore
-    .getAll()
-    .map((cookie) => `${cookie.name}=${cookie.value}`)
-    .join(';');
 
+export const handler = async <T>(path: string, method: 'GET' | 'POST' | 'PATCH' | 'DELETE', options?: RequestInit): Promise<T> => {
   const url = urlJoin(process.env.NEXT_PUBLIC_SERVER_URL || 'https://api.wiscro.app/', path);
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
     'Access-Control-Allow-Credentials': 'true',
-    cookie: cookie,
   };
 
   const init: RequestInit = {
