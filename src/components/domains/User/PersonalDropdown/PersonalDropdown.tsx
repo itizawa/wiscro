94e8a1c0031629e7f2b1eadbffd070e5936ea9b2
itizawa <itizawa.pen@gmail.com>
2023-11-01 00:17:56 +0900

    fix

--- src/components/domains/User/PersonalDropdown/PersonalDropdown.tsx
+++ src/components/domains/User/PersonalDropdown/PersonalDropdown.tsx
@@ -1,13 +1,24 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, useDisclosure } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+import urlJoin from 'url-join';
 import { EditNoteModal } from '../../Note/EditNoteModal';
 import { Icon } from '~/components/uiParts/icons';
 import { User } from '~/domains/User';
 
 export const PersonalDropdown: FC<{ currentUser: User }> = ({ currentUser }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const router = useRouter();
+  useEffect(() => {
+    const get = async () => {
+      const url = urlJoin(process.env.NEXT_PUBLIC_SERVER_URL || 'https://api.wiscro.app/', '/api/me');
+      await fetch(url);
+      router.refresh();
+    };
+    get();
+  }, [router]);
 
   return (
     <>
