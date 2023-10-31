f8e3a6a1f17a451a04a44b85c05d30c003990781
itizawa <itizawa.pen@gmail.com>
2023-11-01 00:21:10 +0900

    Revert "fix"
    This reverts commit 94e8a1c0031629e7f2b1eadbffd070e5936ea9b2.

--- src/components/domains/User/PersonalDropdown/PersonalDropdown.tsx
+++ src/components/domains/User/PersonalDropdown/PersonalDropdown.tsx
@@ -1,24 +1,13 @@
 'use client';
 
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, useDisclosure } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
-import urlJoin from 'url-join';
 import { EditNoteModal } from '../../Note/EditNoteModal';
 import { Icon } from '~/components/uiParts/icons';
 import { User } from '~/domains/User';
 
 export const PersonalDropdown: FC<{ currentUser: User }> = ({ currentUser }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const router = useRouter();
-  useEffect(() => {
-    const get = async () => {
-      const url = urlJoin(process.env.NEXT_PUBLIC_SERVER_URL || 'https://api.wiscro.app/', '/api/me');
-      await fetch(url);
-      router.refresh();
-    };
-    get();
-  }, [router]);
 
   return (
     <>
