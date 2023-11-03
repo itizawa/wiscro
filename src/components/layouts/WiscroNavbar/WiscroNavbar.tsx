d19838b1fcdfb9487173d07ef2459cd86514e5a4
itizawa <itizawa.pen@gmail.com>
2023-11-03 21:36:14 +0900

    Revert "Revert "Revert "Revert "Revert "Revert "WIP""""""
    This reverts commit dc1b5ada46a245578bcea64c6f6efdc5b909309c.

--- src/components/layouts/WiscroNavbar/WiscroNavbar.tsx
+++ src/components/layouts/WiscroNavbar/WiscroNavbar.tsx
@@ -1,15 +1,13 @@
-'use client';
-
 import React, { FC } from 'react';
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/navbar';
 import { Link } from '@nextui-org/link';
-import { Spinner } from '@nextui-org/react';
 import { LoginButton } from '~/components/domains/User/LoginButton';
 import { PersonalDropdown } from '~/components/domains/User/PersonalDropdown';
-import { useCurrentUser } from '~/hooks/user/useCurrentUser';
+import { User } from '~/domains/User';
+import { apiGet } from '~/app/restClient';
 
-export const WiscroNavbar: FC = () => {
-  const { data: currentUser, isLoading } = useCurrentUser();
+export const WiscroNavbar: FC = async () => {
+  const { currentUser } = await apiGet<{ currentUser: User }>('/api/me', { cache: 'no-cache' });
 
   return (
     <>
@@ -20,11 +18,7 @@ export const WiscroNavbar: FC = () => {
           </Link>
         </NavbarBrand>
         <NavbarContent justify="end">
-          {isLoading ? (
-            <Spinner />
-          ) : (
-            <NavbarItem>{currentUser ? <PersonalDropdown currentUser={currentUser} /> : <LoginButton />}</NavbarItem>
-          )}
+          <NavbarItem>{currentUser ? <PersonalDropdown currentUser={currentUser} /> : <LoginButton />}</NavbarItem>
         </NavbarContent>
       </Navbar>
     </>
