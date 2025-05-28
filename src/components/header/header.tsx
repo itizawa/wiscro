bb89bd1b50ed6d72fb5885ea8a572b070eb693c4
itizawa <itizawa.pen@gmail.com>
2025-05-28 23:00:46 +0900

    fix color

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
     }
   };
   return (
-    <header className="bg-white shadow-sm sticky top-0 z-50">
+    <header className="bg-gray-50 shadow-sm sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
